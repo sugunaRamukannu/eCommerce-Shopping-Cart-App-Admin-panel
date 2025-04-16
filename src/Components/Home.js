04817dd00db7098529f947de58d640eab44769a4
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2025-04-16 23:36:24 +0800

    updated view
    updated view

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,7 @@ export default function Products() {
         <>
         <section className="py-5">
          <div className="container p-5 px-md-0 text-center">
-            <h1 className="display-4 fw-bold mt-5">GetFreshFood Admin Portal</h1>
+            <h1 className="display-5 fw-bold">GetFreshFood Admin Portal</h1>
             <p className="fs-3 fw-semibold">Access your products dashboard here!</p>
             <Link className="d-inline-block p-3 btn btn-secondary fw-semibold link-underline link-underline-opacity-0 m-4 rounded-0" to="/admin">Go To Products</Link>
             <div className="border border-black product-dashboard">
